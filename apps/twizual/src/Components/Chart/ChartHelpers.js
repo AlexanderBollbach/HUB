1f5357c95896067f8b2c34640062bd2f80911c7f
AlexanderBollbach <alexander.bollbach@gmail.com>
2018-08-14 22:26:06 -0400

    save

--- apps/twizual/src/Components/Chart/ChartHelpers.js
+++
@@ -1,6 +0,0 @@
-let transString = transform =>
-  `translate(${transform.translation.x} ${transform.translation.y}) scale(${
-    transform.scale.width
-  } ${transform.scale.height})`;
-
-export { transString };
